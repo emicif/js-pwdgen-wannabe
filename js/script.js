1fa931358fd12ab00e7bee10b3e61f6af9764b37
emicif <emanuela.cifolelli@gmail.com>
2022-03-03 15:58:37 +0100

    console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log ('JS OK');
+
+const firstName = prompt ('Qual è il tuo nome?');
+console.log(firstName);
+
+const secondName = prompt ('Qual è il tuo cognome?');
+console.log(secondName);
+
+const color = prompt ('Qual è il tuo colore preferito?');
+console.log(color);
+
+console.log(
+`
+${firstName}${secondName}${color}
+
+`
+);
