d3b57da4548163a07625f7405ceb81cb5360d59d
emicif <emanuela.cifolelli@gmail.com>
2022-03-04 09:54:04 +0100

    backtick

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const firstName = prompt ('Qual è il tuo nome?');
 console.log(firstName);
 
 const lastName = prompt ('Qual è il tuo cognome?');
-console.log(secondName);
+console.log(lastName);
 
 const color = prompt ('Qual è il tuo colore preferito?');
 console.log(color);
